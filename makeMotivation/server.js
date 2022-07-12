ea1f68d181d4a6e2b89d71dd0fdd3b1a1df3d9f7
Mark Linn <marktlinn@gmail.com>
2022-07-12 14:33:49 +0200

    removed password from public access

--- makeMotivation/server.js
+++ makeMotivation/server.js
@@ -2,24 +2,32 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const MongoClient = require("mongodb").MongoClient;
+const MONGO_KEY = require("./keys.json");
+const MONGODB_PWD = MONGO_KEY.MongoDB_Secret;
 const PORT = 3000;
 
 
-MongoClient.connect("mongodb+srv://marktlinn:vuhFiv-7kytto-xufbav@cluster0.nk23j.mongodb.net/motivatorMaker?retryWrites=true&w=majority",
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+MongoClient.connect(`mongodb+srv://marktlinn:${MONGODB_PWD}@cluster0.nk23j.mongodb.net/motivatorMaker?retryWrites=true&w=majority`,
  {useUnifiedTopology: true})
     .then(client => {
         console.log('Connection to database established...')
         const db = client.db("motivatorMaker");
         const quoteCollection = db.collection("motivate");
         app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json())
         app.get("/", (req,res)=>{
             db.collection("motivate").find().toArray()
-            .then(data=> console.log(data.forEach(quote=> console.log(quote.quoteBox))))
+            .then(data=> {
+                res.render("index.ejs", {quotes: data});
+                console.log(data) 
+            })
             .catch (err=> {
                 res.status(400).send("<h1>400: Page Not Found</h1>")
                 console.log(`400 Error: ${err}`)
             })
-            res.sendFile(`${__dirname}/index.html`)
+            // res.sendFile(`${__dirname}/index.html`)
         });
         app.post("/motivate", (req,res)=>{
             let quote = req.body;
@@ -30,6 +38,9 @@ MongoClient.connect("mongodb+srv://marktlinn:vuhFiv-7kytto-xufbav@cluster0.nk23j
             })
             .catch(err=> console.log(`POST Error: ${err}`))
         });
+        app.put("/update", (req,res)=>{
+            console.log(req.body)
+        })
         app.listen(PORT, ()=>{
             console.log(`Listening on port: ${PORT}`);
         });
