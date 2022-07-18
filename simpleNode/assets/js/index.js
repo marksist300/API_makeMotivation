45a307308723cf2e95aef9187128760967018e70
Mark Linn <marktlinn@gmail.com>
2022-07-18 09:29:33 +0200

    updated CSS rule for searchbar input to stop text disappearing behind submit button

--- simpleNode/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('button');
-console.log('howdy, world')
-btn.addEventListener('click', ()=>{
-    document.querySelector('h1').innerText = 'Clicked!!!!!'
-    console.log('hello World')
-})
